a891abee8c47aa9e4e034e3fea641a5177544430
Fabricio Asat <fabricioasat00@gmail.com>
2025-11-14 17:55:44 -0300

    feat: chunks separe

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("motion")) return "motion";
+            if (id.includes("react-router")) return "router";
+            if (id.includes("react")) return "react"; // incluye react y react-dom
+            return "vendor"; // el resto
+          }
+        },
+      },
+    },
+  },
+});
